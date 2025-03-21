001338899441a59dd048c2fe85eef7c807088f1f
Dhanushpthampi <118435199+Dhanushpthampi@users.noreply.github.com>
2025-03-22 02:57:43 +0530

    favicon obviously

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Dhanush P -Portfolio",
   description: "Portfolio of Dhanush P Thampi", 
+  icons: {
+    icon: "/favicon.ico", // Default favicon 
+    shortcut: "/favicon-32x32.png", // Alternative favicon
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body className={inter.className}>
         <TransitionProvider>{children}</TransitionProvider>
       </body>
